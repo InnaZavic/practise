59f7ad581525c66edd3624e2e0074a634cd803cb
InnaZavic <i.zavoiura@gmail.com>
2021-04-04 18:01:31 +0300

    Add vitalikd.sh

---
+++ vitalikd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir mondey
+cd mondey
+mkdir wednesday
+cd wednesday
+mkdir friday thursday tuesday
+cd tuesday
+touch suzuki.txt mazda.txt audi.txt lada.json bmw.json  
+mkdir eat pray love
+ls
+mv bmw.json mazda.txt /home/inna/monday/wednesday/thursday
